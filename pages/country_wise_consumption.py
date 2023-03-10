d40249e5721e6c4bce8dfbda06058d932beed171
prachiti98 <prachitiparkar98@gmail.com>
2023-03-10 07:28:05 -0500

    Added code for fig 16 price left

---
+++ pages/country_wise_consumption.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+"""
+    Created date: 9/12/22
+"""
+
+import yaml
+import numpy as np
+import streamlit as st
+import plotly.graph_objects as go
+from glob import glob
+import os
+import pandas as pd
+from dateutil import parser
+import datetime
+from plotly.subplots import make_subplots
+import environment
+import agent
+import eval_util
+
+def get_datetime(iso_str):
+    dt = parser.parse(iso_str)
+    dt = datetime.datetime.combine(dt.date(), datetime.time(hour=dt.hour))
+    return dt
+
+# Constants
+ds_size_map = {
+    "tinyimagenet": 100000,
+    "imagenet": 1281167,
+}
+
+cpu_power_offset = 50
+
+st.sidebar.markdown("### Country wise plots")
+st.markdown("## C02 consumption and savings")
+
+profile_path = "scale_profile.yaml"
+with open(profile_path, 'r') as f:
+    task_profile = yaml.safe_load(f)
+
+carbon_traces_path = sorted(glob("traces/*.csv"))
+carbon_trace_names = [os.path.basename(trace_name) for trace_name in carbon_traces_path]
+carbon_trace_names = [os.path.splitext(trace_name)[0] for trace_name in carbon_trace_names]
+carbon_trace_map = {trace_name: trace_path for trace_name, trace_path in zip(carbon_trace_names, carbon_traces_path)}
+
+#selected_trace = st.sidebar.selectbox("Carbon Trace", options=carbon_trace_names) #do it for every country
+
+carbon_traces = []
+min_date_value = []
+max_date_value = []
+# print(carbon_trace_names)
+carbon_trace_names_total = ['IE','IN-MH']
+
+carbon_trace_names_test = st.sidebar.multiselect(
+    'Countries',carbon_trace_names_total)
+
+for selected_trace in carbon_trace_names_test:
+    carbon_trace = pd.read_csv(carbon_trace_map[selected_trace])
+
+    carbon_trace = carbon_trace[carbon_trace["carbon_intensity_avg"].notna()]
+    carbon_trace = carbon_trace[carbon_trace["carbon_intensity_avg"].notna()]
+    carbon_trace["hour"] = carbon_trace["zone_datetime"].apply(lambda x: parser.parse(x).hour)
+    carbon_trace["datetime"] = carbon_trace["zone_datetime"].apply(get_datetime)
+    carbon_trace["date"] = carbon_trace["zone_datetime"].apply(lambda x: parser.parse(x).date())
+    carbon_traces.append(carbon_trace)
+    min_date_value.append(carbon_trace["date"].min())
+    max_date_value.append(carbon_trace["date"].max())
+
+
+
+
+selected_task = st.sidebar.selectbox("Task", options=task_profile.keys())
+input_task_length = st.sidebar.number_input("Task Length (hour)", min_value=1, value=24)
+input_deadline = st.sidebar.number_input("Deadline", min_value=input_task_length, value=input_task_length)
+input_max_workers = st.sidebar.number_input("Max Workers", min_value=1, max_value=8, value=8)
+
+
+
+#allow to choose any 24h period - to do
+input_started_date = st.sidebar.date_input("Started Date", min_value=max(min_date_value),
+                                           max_value=min(max_date_value), value=max(min_date_value))
+
+#need to figure out - to do
+# started_datetime_df = carbon_trace[carbon_trace["date"] == input_started_date]
+
+input_started_hour = st.sidebar.number_input("Started Hour", min_value=4,
+                                             max_value=22,
+                                             value=4)
+
+started_hour_time = datetime.time(hour=input_started_hour)
+started_datetime = datetime.datetime.combine(input_started_date, started_hour_time)
+
+
+
+
+sched_fig = make_subplots(specs=[[{"secondary_y": True}]])
+carbon_consumption = []
+
+model_profile = task_profile[selected_task]
+dataset = model_profile["dataset"]
+ds_size = ds_size_map[dataset]
+num_profile = max(model_profile["replicas"])
+
+tp_table = np.zeros(num_profile+1)
+energy_table = np.zeros_like(tp_table)
+
+for num_workers, profile in model_profile["replicas"].items():
+    tp_table[num_workers] = profile["throughput"]
+    energy_table[num_workers] = profile["gpuPower"] + (cpu_power_offset * num_workers)  # Add CPU energy offset
+
+tp_table = tp_table / ds_size  # to epochs per hour
+energy_table = energy_table * 3600. / 3.6e+6   # to Kwh per hour
+num_epochs = tp_table[1] * input_task_length
+
+reward = environment.NonLinearReward(tp_table, energy_table)
+
+for carbon_trace in carbon_traces:
+    # Carbon scale method
+    started_index = carbon_trace.index[carbon_trace["datetime"] == started_datetime][0]
+    env = environment.CarbonOnlyEnvironment(carbon_trace["carbon_intensity_avg"].values,
+                                            reward, np.array([started_index]), num_epochs)
+    carbon_scale_agent = agent.CarbonScaleAgent(tp_table, energy_table, input_max_workers, input_deadline)
+    carbon_cost_scale, carbon_scale_states, carbon_scale_action = \
+        eval_util.simulate_agent(carbon_scale_agent, env, input_deadline)
+
+    # st.metric("Carbon Scale Footprint", f"{carbon_cost_scale[0]:.2f}g")
+
+
+    #to kgs
+    carbon_consumption.append(carbon_cost_scale[0]/1000)
+
+
+sched_fig.add_trace(
+    go.Scatter(x=carbon_trace_names_test,
+               y=carbon_consumption,
+               mode="lines+markers", name="Carbon Intensity"),
+    secondary_y=False
+)
+
+st.plotly_chart(sched_fig)

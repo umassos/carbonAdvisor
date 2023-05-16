b72c5cf8517a148d2bd1aa9aeede07eceae2bd13
Swathi Natarajan <rswathinatraj@gmail.com>
2023-05-15 21:22:08 -0400

    Upload file schema verification and heatmaps for carbon consumption

--- pages/Upload_Files.py
+++ pages/Upload_Files.py
@@ -9,7 +9,10 @@ Version: 1.0
 """
 
 import streamlit as st
+import requests
 import yaml
+import jsonschema
+import json
 from Update_Session import fUpdateSessionDefaultProfile
 
 
@@ -19,38 +22,83 @@ st.markdown("## Upload profile files")
 
 config_session = st.file_uploader("Please upload a file in the right format")
 
+#Show an example profile.yaml in the UploadFiles
+button = st.button("Show sample profile")
+
+if button:
+    with open("profile_sample.yaml", "r") as f:
+        content = yaml.load(f, Loader=yaml.FullLoader)
+        config_session = None
+        st.write(content)
+
+#Basic Schema yaml file structure
+schema = {
+    "type": "object",
+    "patternProperties": {
+        "^\\w+$": {
+            "type": "object",
+            "properties": {
+                "instance": {"type": "string"},
+                "replicas": {
+                    "type": "object",
+                    "patternProperties": {
+                        "^\\d+$": {
+                            "type": "object",
+                            "properties": {
+                                "power": {"type": "number"},
+                                "throughput": {"type": "number"}
+                            },
+                            "required": ["power", "throughput"]
+                        }
+                    }
+                }
+            },
+            "required": ["instance", "replicas"]
+        }
+    },
+    "additionalProperties": False
+}
+
 # Call the library to update the session state "Config_session" if it is not available.
 if "config_session" not in st.session_state:
     fUpdateSessionDefaultProfile()
 
 if config_session is not None:
     temp = yaml.safe_load(config_session)
-    
     try:
+        #Schema validation using validate() function
+        json_content = json.dumps(temp)
+        jsonschema.validate(json.loads(json_content), schema)
+        
+        #Obtaining the instance types from AWS API and using it to validate values of "instance"
+        api_url = "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/ec2/USD/current/ec2-ondemand-without-sec-sel/US%20East%20(Ohio)/Linux/index.json"
+        response = requests.get(api_url)
+        res = response.json()
+        regions = list(res["regions"].keys())
+        instances = set()
+        for region in regions:
+            confs = list(res["regions"][region].keys())
+            for conf in confs:
+                instances.add(res["regions"][region][conf]["Instance Type"])
+
+        #Custom Validations
         #validation of the new uploaded yaml file:
         for model in temp:
-            #1. Check if the task profile in the uploaded file exists in schema
-            assert model not in st.session_state["config_session"].keys(), "Keys already found in the existing profile"
-            
-            #2. For a particular task profile, check if 'instance' exists and has the value same as the type of schema
-            assert 'instance' in temp[model], "instance type key is missing in the uploaded file" 
-            ## todo: check if instance is correct
-            
-            #3.For a particular task profile, check if 'replicas' exists and has the same number of replicas as schema.
-            assert 'replicas' in temp[model], "replicas key is missing in the uploaded file"
-            assert temp[model]['replicas'] is not None, "Replica list is empty. Minimum 1 replicas should be present"
-            assert list(range(min(temp[model]['replicas'].keys()), max(temp[model]['replicas'].keys())+1)) == list(temp[model]['replicas'].keys()), "The replica numbers are not continuous"
+            #1. If the model already exists
+            #assert model not in st.session_state["config_session"].keys(), "Keys already found in the existing profile"
 
-            #4. For each replica, check if 'power' and 'throughput' exists and has the value same as the type of schema["densenet121" replica 1 is taken as schema reference] TBD:: This can be improved by introducing a schema file
-            for replica in temp[model]['replicas']:
-                assert 'power' in temp[model]['replicas'][replica], "power key is missing under Replicas"
-                assert isinstance(temp[model]['replicas'][replica]['power'], type(st.session_state["config_session"]["densenet121"]['replicas'][1]['power'])) == True, "power key has value of different datatype"
-                assert 'throughput' in temp[model]['replicas'][replica], "throughput key is missing under Replicas"
-                assert isinstance(temp[model]['replicas'][replica]['throughput'], type(st.session_state["config_session"]["densenet121"]['replicas'][1]['throughput'])) == True, "throughput key has value of different datatype"
+            #2. Check if the given instance is among the AWS approved instances
+            assert temp[model]["instance"] in instances, "{} is NOT among the AWS approved instances".format(temp[model]["instance"])
+            
+            #3. check if 'replicas' values are continous.
+            assert list(range(min(temp[model]['replicas'].keys()), max(temp[model]['replicas'].keys())+1)) == list(temp[model]['replicas'].keys()), "The replica numbers are not continuous"
 
         st.session_state["config_session"].update(temp)
         st.success("Uploaded successfully!")
         st.write(temp)
     except AssertionError as warn:
+        st.error("Upload Failed: The file schema is invalid.")
+        st.error(warn)
+    except jsonschema.ValidationError as warn:
         st.error("Upload Failed: The file schema is invalid.")
         st.error(warn)

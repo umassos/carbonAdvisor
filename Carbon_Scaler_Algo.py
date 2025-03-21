f30d38ff3ef1be7b04edb81f72e9b4b3bfd545d9
Roshini Sanikop <1by19is190@bmsit.in>
2025-03-21 01:13:56 -0400

    consolidated functions

--- Carbon_Scaler_Algo.py
+++ Carbon_Scaler_Algo.py
@@ -42,6 +42,40 @@ class CarbonScalerAlgo(CarbonAdvisorMethods):
         self.task_schedule = task_schedule
         return task_schedule
     
+    def get_scale_at_a_time(self, task_schedule):
+        # Extract the number of worker nodes assigned at each time step. one time step = hour
+        total_work = 0
+        # total_samples = int(self.task_length * self.task_profile[1]["throughput"] * 3600)  # job unable to finish. 
+        total_samples = int(self.task_length * self.task_profile[1]["throughput"])
+    
+        print(f"Required Total Work (total_samples): {total_samples}")
+
+        for time, nodes in task_schedule.items():
+            work_done = self.task_profile[nodes]["throughput"] * 3600
+            total_work += work_done
+
+            print(f"Time step: {time}, No. of workers: {nodes}, Work done: {work_done}, Total amount of work done: {total_work}")
+
+            if total_work >= total_samples:
+                print(f"Job completes at time step {time}")
+                return time, {t: n for t, n in task_schedule.items()}
+        
+        print("Job did not complete within given task schedule.")
+        return None, {t: n for t, n in task_schedule.items()} 
+
+    def get_total_emission(self, task_schedule):
+        
+        # return sum(self.carbon_trace.iloc[t]["carbon_intensity_avg"] * self.task_profile[nodes]["power"] for t, nodes in task_schedule.items())
+        emissions = []
+        for t, nodes in task_schedule.items():
+            emissions.append(self.carbon_trace.iloc[t]["carbon_intensity_avg"] * self.task_profile[nodes]["power"])
+        print("testing if code is working")
+        return sum(emissions)
+
+    def get_total_energy(self, task_schedule):
+        return sum(self.task_profile[nodes]["power"] for nodes in task_schedule.values())
+
+
     def get_compute_time(self):
         print("in get compute time")
         task_throughput = 0
@@ -66,10 +100,32 @@ class CarbonScalerAlgo(CarbonAdvisorMethods):
         return total_emissions
 
 
-
-
+    def analyse_schedule(self):
+        # Runs all the functions and return results.
+       
+        task_schedule = self.compute_schedule()
+        completion_time, scale_at_time = self.get_scale_at_a_time(task_schedule)
+        return {
+            "completion_time": completion_time,
+            "scale_at_time": scale_at_time,
+            "total_emission": self.get_total_emission(task_schedule),
+            "total_energy": self.get_total_energy(task_schedule)
+        }
+    
 scaler = CarbonScalerAlgo(deadline=50, slack=8, num_workers=8, task_length=24, location='AU-SA', task='densenet121', start_date='2021-03-22', start_hour=8)
 scaler.compute_schedule()
 print("printing compute time", scaler.get_compute_time())
 scaler.get_total_emissions()
 
+# ---------- Roshini 
+
+scaler1 = CarbonScalerAlgo(deadline=50, slack=8, num_workers=8, task_length=24, location='AU-SA', task='densenet121', start_date='2021-03-22', start_hour=8)
+result1 = scaler1.analyse_schedule()
+print(result1)
+# scaler2 = CarbonScalerAlgo(deadline=50, slack=8, num_workers=8, task_length=24, location='SE-SE1', task='resnet18', start_date='2020-01-01', start_hour=12)
+# scaler.carbon_scaler_algo()
+# result2 = scaler2.analyse_schedule()
+
+
+# print(result2)
+# # print(results)
